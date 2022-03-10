c030655b44763a4394bfe59163ddbe4ca7ce47dd
André Ferreira <tun.ultimate.ninja@gmail.com>
2022-03-10 13:49:09 +0000

    Luz

--- Assets/Luz.cs
+++ Assets/Luz.cs
@@ -6,15 +6,26 @@ public class Luz : MonoBehaviour
 {
     float speed = 10f;
 
+    Color color1 = Color.yellow;
+    Color color2 = Color.white;
+    Color color3 = new Color(1.0f, 0.64f, 0.0f);
 
-   void Start()
+    Light luz;
+    void Start()
     {
- 
+        luz = GetComponent<Light>();
     }
 
     void Update()
     {
         if (transform.eulerAngles.x <= 160)
-        transform.Rotate(Vector3.right * Time.deltaTime * speed);
+            transform.Rotate(Vector3.right * Time.deltaTime * speed);
+
+
+        float t = Mathf.PingPong(Time.time, speed) / speed;
+        luz.color = Color.Lerp(color1, color2, t);
+
+        if( transform.eulerAngles.x >= 80)
+        luz.color = Color.Lerp(color2, color3, t);
     }
 }
