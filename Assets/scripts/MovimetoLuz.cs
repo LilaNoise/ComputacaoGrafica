08aaa3a06bdb20ce1567223c273eba91736c1d64
André Ferreira <tun.ultimate.ninja@gmail.com>
2022-03-24 12:55:35 +0000

    Aula_4
    Pacman (Inacabado)

---
+++ Assets/scripts/MovimetoLuz.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentp : MonoBehaviour
+{
+    float speed;
+    Vector3 target1;
+
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 3, 5);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
